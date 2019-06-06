ff9aad8310047a72a2e01dd5c1f35f5f1b44790a
Chen Yujing <vit7david@outlook.com>
2019-06-06 23:03:06 +0300

    Router and snapshot testing without rendering entire App.js with route

--- src/App.js
+++ src/App.js
@@ -1,15 +1,19 @@
 import React from "react";
-import HeaderNav from "./containers/HeaderNav/HeaderNav";
-import SideBar from "./containers/SideBar/SideBar";
+import AppLayout from "./components/AppLayout/AppLayout";
 import Home from "./containers/Home/Home";
+import Watch from "./containers/Watch/Watch";
+import { Route, Switch } from "react-router-dom";
 import "./App.scss";
 
 function App() {
   return (
-    <React.Fragment>
-      <HeaderNav />
-      <Home />
-    </React.Fragment>
+    <AppLayout>
+      <Switch>
+        <Route path="/watch" component={Watch} />
+        <Route path="/" component={Home} />
+        {/* if use path instead of exact path, order of route matters, if switch line 13, 14, it will always end up in home component */}
+      </Switch>
+    </AppLayout>
   );
 }
 
---
+++ src/containers/Watch/Watch.js
@@ -0,0 +1,7 @@
+import React, { Component } from "react";
+import "./Watch.scss";
+export default class Watch extends Component {
+  render() {
+    return <div>Placeholders</div>;
+  }
+}
