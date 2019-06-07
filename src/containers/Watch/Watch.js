3ea54cdf654925a1c05485b0e35e1ba778c1cfa8
Chen Yujing <vit7david@outlook.com>
2019-06-07 11:37:48 +0300

    Video component and snapshot testing

--- src/containers/Watch/Watch.js
+++ src/containers/Watch/Watch.js
@@ -1,7 +1,12 @@
 import React, { Component } from "react";
 import "./Watch.scss";
+import Video from "../../components/Video/Video";
 export default class Watch extends Component {
   render() {
-    return <div>Placeholders</div>;
+    return (
+      <div style={{ maxWidth: "80%" }}>
+        <Video id="-7fuHEEmEjs" />
+      </div>
+    );
   }
 }
