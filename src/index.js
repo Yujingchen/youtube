03bdd8eb09ed435e489cf3b253f048eca7399ef4
Chen Yujing <vit7david@outlook.com>
2019-06-07 22:48:34 +0300

    set up redux store and redux saga

--- src/index.js
+++ src/index.js
@@ -3,11 +3,17 @@ import ReactDOM from "react-dom";
 import App from "./App";
 import * as serviceWorker from "./serviceWorker";
 import "semantic-ui-css/semantic.min.css";
+import { Provider } from "react-redux";
 import { BrowserRouter } from "react-router-dom";
+import { configureStore } from "./store/configureStore";
+
+const store = configureStore;
 ReactDOM.render(
-  <BrowserRouter>
-    <App />
-  </BrowserRouter>,
+  <Provider store={store}>
+    <BrowserRouter>
+      <App />
+    </BrowserRouter>
+  </Provider>,
   document.getElementById("root")
 );
 
